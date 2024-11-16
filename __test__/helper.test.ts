9117dc82ff15a3dd2a3f989d12ded34b90f97b99
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-16 17:46:01 +0100

    test

--- __test__/helper.test.ts
+++ __test__/helper.test.ts
@@ -14,9 +14,18 @@ describe('helper', () => {
         expect(helper).toBeTruthy();
     })
 
-    it('', () => {
-        wrapper.style.width = Number(wrapper.style.width) + 1 + 'px';
+    it('has reference to model', () => {
+        const initial = { x: 0, y: 0 };
+        const { model } = createHelper(wrapper, initial);
 
-        expect(true).toBeTruthy();
+        expect(model).toBeTruthy();
+        expect(model).toEqual(initial);
+    })
+
+    it('has reference to #setScene method', () => {
+        const initial = { x: 0, y: 0 };
+        const { setScene } = createHelper(wrapper, initial);
+
+        expect(setScene).toBeTruthy();
     })
 })
