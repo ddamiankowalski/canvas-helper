ea183d9f0a765e35f9df68ab210396424e22a680
Damian Kowalski <dkowalski.1997@gmail.com>
2026-04-19 06:37:01 +0200

    added helper

---
+++ demo/main.ts
@@ -0,0 +1,4 @@
+import { CavnvasHelper } from './../src/helper/canvas-heper';
+
+const wrapper = document.createElement('div');
+new CavnvasHelper(wrapper);
---
+++ src/helper/canvas-heper.ts
@@ -0,0 +1,5 @@
+export class CavnvasHelper {
+  constructor(private _wrapper: HTMLDivElement) {
+    console.log('I am helper!', this._wrapper);
+  }
+}
