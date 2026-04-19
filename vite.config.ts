eb9f3bc026aa23616e71718c38eb38c94a11c73c
Damian Kowalski <dkowalski.1997@gmail.com>
2026-04-19 06:14:41 +0200

    added vitest config

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+/**
+ * Custom vitest config
+ *
+ * @reference https://vitest.dev/config/
+ */
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'CanvasHelper',
+      formats: ['es', 'cjs'],
+      fileName: (format) => {
+        if (format === 'es') {
+          return 'esm/index.js';
+        }
+
+        return 'cjs/index.cjs';
+      },
+    },
+    emptyOutDir: true,
+  },
+});
