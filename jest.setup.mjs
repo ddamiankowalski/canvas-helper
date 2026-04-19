ded995f4fa19f284944dfd840c86bcbc1bcf2c2d
Damian Kowalski <dkowalski.1997@gmail.com>
2026-04-19 06:05:17 +0200

    removed dead

--- jest.setup.mjs
+++
@@ -1,7 +0,0 @@
-const canvasMock = require('jest-canvas-mock');
-global.ResizeObserver = require("resize-observer-polyfill");
-
-beforeEach(() => {
-    jest.resetAllMocks();
-    canvasMock.setupJestCanvasMock();
-});
